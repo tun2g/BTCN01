aefc8a9e864f27f40da4f523ec048142e107481e
Nguyen Thanh Tung <tunghcltt.@gmail.com>
2022-10-02 10:25:37 +0700

    handle button

--- main.js
+++ main.js
@@ -6,17 +6,14 @@ $(".menu").hover(function(){
 
 
 const validation= function(){
-    function reset(id){
-        $(id).click(function(){
-            $(this).parent().parent().children().eq(1).text("")
+    function reset(ids){
+        Array.from(ids).forEach(id=> {
+            $(id).click(function(){
+                $(this).parent().parent().children().eq(1).text("")
+        });
         })
     }
-    reset("#email")
-    reset("#code")
-    reset("#fullname")
-    reset("#address")
-    reset("#phone")
-    reset("#birthday")
+    reset(["#email","#code","#fullname","#address","#phone","#birthday"])
 
     $("#register").click(function(e){
         e.preventDefault()
@@ -27,6 +24,21 @@ const validation= function(){
         if(validation.isEmail("#email")){
             $("#email").parent().parent().children().eq(1).text(validation.isEmail('#email'))
         }
+        if(validation.isBirthday('#birthday')){
+            $("#birthday").parent().parent().children().eq(1).text(validation.isBirthday('#birthday'))
+        }
+        if(validation.isTwoWords('#fullname')){
+            $("#fullname").parent().parent().children().eq(1).text(validation.isTwoWords('#fullname'))
+        }
+        if(validation.isTwoWords('#address')){
+            $("#address").parent().parent().children().eq(1).text(validation.isTwoWords('#address'))
+        }
+        if(validation.isPhone('#phone')){
+            $("#phone").parent().parent().children().eq(1).text(validation.isPhone('#phone'))
+        }
+        if(validation.isChecked()){
+            $("#male").parent().parent().parent().children().eq(1).text(validation.isChecked())
+        } 
     })
 }
 validation.isCode=function(id){
@@ -40,5 +52,59 @@ validation.isEmail=function(id){
     var regex=new RegExp( /^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,}){1,3}$/)
     return regex.test(vl) ? undefined :  'Email không hợp lệ'    
 }
-
+validation.isBirthday=function(id){
+    value=$(id).val()
+    const regex=new RegExp(/^([12][0-9]|0[1-9]|3[01])[/](0[1-9]|1[0-2])[/](200[0-4]|1999)$/)
+    return regex.test(value) ? "" :  'Ngày sinh Không hợp lệ'
+}
+validation.isTwoWords=function(id){
+    value=$(id).val()
+    const regex=new RegExp(/[A-Za-z0-9]{1,}\s[A-Za-z0-9]{1,}$/)
+    return regex.test(value) ? "" :  'Phải có ít nhất 2 từ'
+}
+validation.isPhone=function(id){
+    value=$(id).val()
+    const regex = new RegExp(/^[0]+[0-9]{9}/);
+    return regex.test(value) ? "" :  'Số điện thoại không hợp lệ'
+}
+validation.isChecked=function(){
+    const checked = $('input[name=sex]:checked').length;
+    return checked!==0 ? "" :'Chưa chọn giới tính'
+}
 validation()
+
+$(".sub").click(function(){
+    if($(this).hasClass('sub-active'))
+        $(this).removeClass('sub-active')
+    else $(this).addClass('sub-active')
+})
+
+$("#list-btn1").click(()=>{
+    $('#list2').append($("#list1").children('.sub-active'))
+    $('#list2').find('.sub-active').removeClass('sub-active')
+})
+$("#list-btn2").click(()=>{
+    $('#list2').append($("#list1").children())
+    $('#list1').find('.sub-active').removeClass('sub-active')
+    $('#list2').find('.sub-active').removeClass('sub-active')
+})
+$("#list-btn3").click(()=>{
+    $('#list1').append($("#list2").children('.sub-active'))
+    $('#list1').find('.sub-active').removeClass('sub-active')
+})
+$("#list-btn4").click(()=>{
+    $('#list1').append($("#list2").children())
+    $('#list1').find('.sub-active').removeClass('sub-active')
+    $('#list2').find('.sub-active').removeClass('sub-active')
+})
+$(".s-n-header").click(function(){
+    if($(this).hasClass('shrink')){
+        $(this).removeClass('shrink')
+        $(this).parent().children().eq(1).removeClass("dnone")
+    }
+    else {
+        $(this).addClass('shrink')
+        $(this).parent().children().eq(1).addClass("dnone")
+    
+    }
+})
